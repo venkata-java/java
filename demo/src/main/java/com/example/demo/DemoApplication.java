400c9ce185b9743e1b1a78705c7fef9089f9bed1
amma <amma@amma-PC>
2020-02-29 11:31:23 +0530

    modified files

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 	
 	@RequestMapping("/")
     String home() {
-        return "Hello World Spring Boot!";
+        return "Hello World Spring ";
     }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
